74978addc91e20b43a38bc5ddf8c76935aae2989
Creative-banda <4kpuspa@gmail.com>
2025-04-02 17:29:06 +0530

    Added Touch swipe on the display to control player instead of keyboard buttons

--- scenes/audio_controller.gd
+++ scenes/audio_controller.gd
@@ -7,12 +7,11 @@ func play_jump() -> void:
 func play_hurt() -> void: 
 	$Hurt.play()
 	
-	
 func play_level_complete() -> void:
 	$Level_complete.play()
 
 func play_level1() -> void:
 	$level_1.play()
-	
+
 func play_collect_item() -> void:
 	$collect_item.play()
--- scenes/end.gd
+++ scenes/end.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+@export var scene_change : PackedScene
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "CharacterBody2D":
 		call_deferred("change_scene")
+		AudioController.play_level_complete()
 
 func change_scene() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+	get_tree().change_scene_to_packed(scene_change)
